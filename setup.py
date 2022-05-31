88d37cece343fe066478ed3af3d4e1d862135fcd
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2022-05-31 22:05:54 +0300

    added statsmodels

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description="A/B tools",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=["hdbscan == 0.8.26",
+    install_requires=["hdbscan == 0.8.26","statsmodels == 0.12.2",
         "numpy == 1.20.3", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
         "SQLAlchemy >= 1.1.15", "pytest >= 6.2", "PyYAML >= 5.4.1", "scikit_learn >= 0.24.2", 
         "fastcore ==  1.4.3"
