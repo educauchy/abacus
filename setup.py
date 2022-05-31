cff9606914972a254ed4c83d9c05afb45b573810
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2022-05-31 18:23:31 +0300

    cython added 1

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     install_requires=[
         "numpy >= 1.18.5", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
         "SQLAlchemy >= 1.1.15", "pytest >= 6.2", "PyYAML >= 5.4.1", "scikit_learn >= 0.24.2", "hdbscan == 0.8.26",
-        "fastcore ==  1.4.3", "Cython == 0.29.24"
+        "fastcore ==  1.4.3", "cython == 0.29.24"
     ],
 )
