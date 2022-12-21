85e919746dce1bda4b4cfbd4cb49221a72248730
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2022-12-21 17:51:14 +0300

    legacy deleted

--- auto_ab/prepilot_spark/runners/test.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-def main(args: argparse.Namespace):
-    pass
-
-if __name__ == "__main__":
-    args_parser = argparse.ArgumentParser(description="test")
-    
--- auto_ab/utils/errors.py
+++
@@ -1,3 +0,0 @@
-class AbsentColsError(Exception):
-    """Any columns don't exist in DataFrame
-    """
