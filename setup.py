52c49780e2331453b6d4170a8ee53a8b4ff242d7
Egor Shishkovets <egorshishkovets@MacBook-Air-Egor.local>
2023-01-09 18:41:03 +0300

    add joblib install stage

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=["hdbscan == 0.8.28","statsmodels == 0.13.2","tqdm == 4.62.3",
-        "numpy == 1.20.3", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
+        "numpy == 1.22.0", "pandas >= 1.2.4", "pydantic >= 1.8.1", "scipy >= 1.4.1",
         "SQLAlchemy >= 1.1.15", "pytest >= 6.2", "PyYAML >= 5.4.1", "scikit_learn >= 0.24.2", 
         "fastcore ==  1.4.3", "matplotlib == 3.4.3", "hyperopt == 0.2.7",
         "seaborn == 0.11.2", "Jinja2 == 2.11.3", "category_encoders==2.4.1"
