edbb574797b56f0619bea45f330691267e11c630
Egor Shishkovets <egor.shishkovets@glowbyteconsulting.com>
2023-07-04 18:33:42 +0300

    split builder docs updated

--- abacus/splitter/params.py
+++ abacus/splitter/params.py
@@ -6,16 +6,32 @@ from pydantic import validator
 
 @dataclass
 class SplitBuilderParams:
+    """Split experiment parameters class.
+
+    Args:
+        map_group_names_to_sizes(Dict): dictionary with group names and sizes. 
+            Key with name "control" is obligatory
+        main_strata_col (str): nthe name of the column to be used first for splitting
+        split_metric_col (str): the name of the column to be binning data for splitting
+        id_col (str): the name of the column with id 
+        cols: columns for stratification data
+        cat_cols: categorical columns that are using for stratification.
+            These cols'll be encoded as category features
+        n_bins: number of bins to be created based on split_metric_col
+        min_cluster_size: min count of samples in HDBSCAN cluster
+        strata_outliers_frac: frequency of outlyers in strata
+        alpha: significance level for A/A test for split
+    """
     min_unique_values_in_col: ClassVar[int] = 3
     control_group_name: ClassVar[str] = "control"
     map_group_names_to_sizes: Dict[str, Optional[int]]
     main_strata_col: str
     split_metric_col: str
     id_col: str = "customer_id"
-    cols: List[str] = Field(default_factory=list)  # all cols for stratification
-    cat_cols: List[str] = Field(default_factory=list) # this cols'll be encoded as category features
+    cols: List[str] = Field(default_factory=list)
+    cat_cols: List[str] = Field(default_factory=list)
     n_bins: int = 3
-    min_cluster_size: int = 100  # the number of top categories (by frequency) that will not be combined in 'other' categories
+    min_cluster_size: int = 100 
     strata_outliers_frac: float = 0.01
     alpha: float = 0.05
 
